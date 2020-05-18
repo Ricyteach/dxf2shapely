fc6de4e05cbb77e77a5866afdb69cc817bb67027
Rick Teachey <ricky@teachey.org>
2020-05-18 16:39:15 -0400

    Fixed rounding problems

--- dxf2shapely/d2s.py
+++ dxf2shapely/d2s.py
@@ -7,10 +7,10 @@ def iter_line_coord_pairs(space):
     dxf_lines = list(e for e in space if e.dxftype() == 'LINE')
     yield from ([
             list(
-                round(c, 3) for c in l.dxf.start[:-1]
+                round(c, 2) for c in l.dxf.start[:-1]
             ),
             list(
-                round(c, 3) for c in l.dxf.end[:-1]
+                round(c, 2) for c in l.dxf.end[:-1]
             )
         ] for l in dxf_lines)
 
